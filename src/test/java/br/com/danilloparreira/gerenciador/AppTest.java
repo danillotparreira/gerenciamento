2c6fcc8d24939d4e6ea02b948b4bfd7893f3c029
parreira16 <parreira16@gmail.com>
2019-12-14 20:13:14 -0300

    projeto inicial

---
+++ src/test/java/br/com/danilloparreira/gerenciador/AppTest.java
@@ -0,0 +1,12 @@
+package br.com.danilloparreira.gerenciador;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
