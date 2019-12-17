89403b7bfe90009d69f69e0e36192a1b191eaf0f
parreira16 <parreira16@gmail.com>
2019-12-17 01:35:51 -0200

    Criado o modelo de perfil e os DAO de perfil

---
+++ src/main/java/br/com/danilloparreira/gerenciador/dao/PerfilDao.java
@@ -0,0 +1,8 @@
+package br.com.danilloparreira.gerenciador.dao;
+
+import br.com.danilloparreira.gerenciador.dao.generic.GenericDao;
+import br.com.danilloparreira.gerenciador.model.Perfil;
+
+public interface PerfilDao extends GenericDao<Perfil> {
+
+}
