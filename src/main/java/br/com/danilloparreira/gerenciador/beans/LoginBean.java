b94edafca1b0c4f82eddf0a953eb3ae459c59c0a
Danillo <Danillo@192.168.15.5>
2020-02-26 22:43:07 -0300

    Configurado alteração de dados do usuário logado de nome, email e senha.

--- src/main/java/br/com/danilloparreira/gerenciador/beans/LoginBean.java
+++ src/main/java/br/com/danilloparreira/gerenciador/beans/LoginBean.java
@@ -1,5 +1,6 @@
 package br.com.danilloparreira.gerenciador.beans;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -9,6 +10,7 @@ import br.com.danilloparreira.gerenciador.dao.UsuarioDaoImpl;
 import br.com.danilloparreira.gerenciador.model.CadastroAcao;
 import br.com.danilloparreira.gerenciador.model.RelatorioAcao;
 import br.com.danilloparreira.gerenciador.model.Usuario;
+import br.com.danilloparreira.gerenciador.utils.UtilSecurity;
 
 @SessionScoped
 @ManagedBean(name = "loginbean")
@@ -19,9 +21,18 @@ public class LoginBean extends AbstractBean {
 	private String login;
 	private String senha;
 	private boolean loggedIn = false;
+	private String nome;
+	private String email;
+	private String confirmacaoEmail;
+	private String senhanova;
+	private String confirmacaoSenha;
 
 	public LoginBean() {
 		super("Acesso ao Sistema");
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 	public Usuario getUsuario() {
@@ -64,6 +75,85 @@ public class LoginBean extends AbstractBean {
 
 	public boolean isLoggedIn() {
 		return loggedIn;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getConfirmacaoEmail() {
+		return confirmacaoEmail;
+	}
+
+	public void setConfirmacaoEmail(String confirmacaoEmail) {
+		this.confirmacaoEmail = confirmacaoEmail;
+	}
+
+	public String getConfirmacaoSenha() {
+		return confirmacaoSenha;
+	}
+
+	public void setConfirmacaoSenha(String confirmacaoSenha) {
+		this.confirmacaoSenha = confirmacaoSenha;
+	}
+
+	public String getSenhanova() {
+		return senhanova;
+	}
+
+	public void setSenhanova(String senhanova) {
+		this.senhanova = senhanova;
+	}
+
+	public String salvar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		Boolean changed = false;
+		String msg = "";
+
+		if (this.nome != null) {
+			this.usuario.setNome(this.nome);
+			changed = true;
+		} else if (UtilSecurity.convertStringToMd5(this.senha).equals(this.usuario.getSenha())) {
+			if (this.email != null && this.confirmacaoEmail != null && this.senha != null) {
+				if (this.email.trim().equals(this.confirmacaoEmail.trim())) {
+					this.usuario.setEmail(this.email);
+					changed = true;
+				} else {
+					msg = "Os emails não são iguais.";
+				}
+			}
+			if (this.senhanova != null && this.confirmacaoSenha != null) {
+				if (this.senhanova.equals(this.confirmacaoSenha)) {
+					this.usuario.setSenha(UtilSecurity.convertStringToMd5(this.senhanova));
+					changed = true;
+				} else {
+					msg = "Verifique se seus endereços de e-mail são iguais e tente novamente.";
+				}
+			}
+		} else {
+			msg = "Ocorreu um problema com sua Senha";
+		}
+		if (changed) {
+			Usuario usersave = usuarioDao.merge(this.usuario);
+			this.usuario = usersave;
+			return "conta.jsf";
+		} else {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO , "Erro ao salvar", msg));
+			return "";
+		}
 	}
 
 	public String doLogin() {
